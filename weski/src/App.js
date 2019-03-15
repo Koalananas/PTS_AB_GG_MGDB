7c82ecbd7942ed0b05fca39b62c6aead2dd8dcdc
Alexandre Broutin <a.broutin@live.fr>
2019-03-15 16:48:14 +0100

    Yarn

--- weski/src/App.js
+++ weski/src/App.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';s
 import './Styles/App.css';
 import InputCase from './Components/InputCase'
 
-class App extends Component {
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+  };
+    this.callMamp = this.callMamp.bind(this);
+}
+
+callMamp(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          // Typical action to be performed when the document is ready:
+          document.getElementById("demo").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  
+    return <div>xhttp</div>;
+  } 
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <InputCase/>
+          {this.callMamp("http://localhost")}     
         </header>
 
       </div>
--- weski/src/Components/InputCase.js
+++ weski/src/Components/InputCase.js
@@ -6,13 +6,26 @@ class InputCase extends Component {
       super(props);
       this.state = {
         start: 1,
-        end: 5
+        end: 5, 
+        methode: 'BrutPower'
       };
   
-      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleOriginChange = this.handleOriginChange.bind(this);
+      this.handleDestinatioonChange = this.handleDestinatioonChange.bind(this);
+      this.handleJourneyTypeChange = this.handleJourneyTypeChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
   
-    handleInputChange(event) {
+    handleOriginChange(event) {
+      const target = event.target;
+      const startInput = target.type === 'number' ? target.checked : target.value;
+      const name = target.name;
+  
+      this.setState({
+        [name]: startInput
+      });
+    }
+    handleDestinatioonChange(event) {
       const target = event.target;
       const startInput = target.type === 'number' ? target.checked : target.value;
       const name = target.name;
@@ -22,26 +35,48 @@ class InputCase extends Component {
       });
     }
 
+    handleJourneyTypeChange(event) {
+      this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert('Your favorite flavor is: ' + this.state.value);
+      event.preventDefault();
+    }
+
     render() {
       return (
         <form>
           <label>
-            Is going:
+            Origine :  
             <input
               name="start"
               type="number"
               checked={this.state.isGoing}
-              onChange={this.handleInputChange} />
+              placeholder={'Chiffres entre 1 et 20'}
+              onChange={this.handleOriginChange} />
           </label>
           <br />
           <label>
-            Number of guests:
+            Destination : 
             <input
               name="end"
               type="number"
               value={this.state.numberOfGuests}
-              onChange={this.handleInputChange} />
+              placeholder={'Chiffres entre 1 et 20'}
+              onChange={this.handleDestinatioonChange} />
           </label>
+          <br />
+          <label>
+            Type de trajet : 
+            <select value={this.state.value} onChange={this.handleJourneyTypeChange}>
+              <option value="BrutPower">Tous chemins</option>
+              <option value="Dijkstra">Le plus rapide</option>
+              <option value="FordFerkuson">Le plus court</option>
+            </select>
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
         </form>
       );
     }
