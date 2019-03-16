978d5040e69d03407ff8f58c4337f08fc04d3241
Alexandre Broutin <a.broutin@live.fr>
2019-03-16 22:16:44 +0100

    change

--- weski/src/App.js
+++ weski/src/App.js
@@ -1,38 +1,22 @@
-import React, { Component } from 'react';s
+import React, { Component } from 'react';
 import './Styles/App.css';
 import InputCase from './Components/InputCase'
 
 
-export default class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
   };
-    this.callMamp = this.callMamp.bind(this);
 }
 
-callMamp(url) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          // Typical action to be performed when the document is ready:
-          document.getElementById("demo").innerHTML = xhttp.responseText;
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
-  
-    return <div>xhttp</div>;
-  } 
-
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <InputCase/>
-          {this.callMamp("http://localhost")}     
         </header>
-
       </div>
     );
   }
--- weski/src/Components/InputCase.js
+++ weski/src/Components/InputCase.js
@@ -5,54 +5,111 @@ class InputCase extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        start: 1,
-        end: 5, 
-        methode: 'BrutPower'
+        start: '',
+        end: '',
+        methode: '',
+        //isEnabled:  false
       };
   
       this.handleOriginChange = this.handleOriginChange.bind(this);
       this.handleDestinatioonChange = this.handleDestinatioonChange.bind(this);
       this.handleJourneyTypeChange = this.handleJourneyTypeChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.parseUrl  = this.parseUrl.bind(this);
+      this.callMamp  = this.callMamp.bind(this);
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if((this.state.start != prevState.start) || (this.state.end != prevState.end) || (this.state.methode != prevState.methode))
+      {
+        this.setState({
+          
+        })
+      }
+    }
+
+
+    parseUrl(start,end, methode){
+      let url = "http://localhost:8888/PTS_AB_GG_MGDB/Calcul/jsonResult.php?"
+      if(start > 0 && start < 25) {
+        url += 'start=' + start + '&';
+      }
+      if(end > 0 && end < 25) {
+        url += 'end=' + end + '&';
+      }
+      if(methode !== null){
+        if(methode === 'Dijkstra'){   
+          url += 'methode=d';
+        }
+        else if(methode === 'FordFerkuson'){
+          url += 'methode=f';      
+        }
+        else {
+          url += 'methode=b';
+        }
+      }
+      const jsonResult = this.callMamp(url);
+      console.log('url',url);
+      return jsonResult;
+    }
+
+    callMamp(url) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", url, false);
+      xhttp.send();
+      const http = xhttp;
+      console.log('xhttp',http);
+
+  
+      var obj = xhttp.responseText;
+      
+      return obj;
     }
   
     handleOriginChange(event) {
-      const target = event.target;
-      const startInput = target.type === 'number' ? target.checked : target.value;
-      const name = target.name;
+      const startInput = event.target.value;
   
       this.setState({
-        [name]: startInput
+        start: startInput
       });
     }
     handleDestinatioonChange(event) {
-      const target = event.target;
-      const startInput = target.type === 'number' ? target.checked : target.value;
-      const name = target.name;
+      const startInput = event.target.value;
   
       this.setState({
-        [name]: startInput
+        end: startInput
       });
     }
 
     handleJourneyTypeChange(event) {
-      this.setState({value: event.target.value});
+      this.setState({methode: event.target.value});
     }
 
     handleSubmit(event) {
-      alert('Your favorite flavor is: ' + this.state.value);
-      event.preventDefault();
+      const {start, end, methode} = this.state;
+      const json = this.parseUrl(start,end,methode);
+      console.log(json);
     }
 
     render() {
+      
+      const {start, end, methode} = this.state;
+      console.log(this.state)
+      const json = this.parseUrl(start,end,methode);
+      console.log(json);
+
       return (
         <form>
           <label>
             Origine :  
             <input
-              name="start"
-              type="number"
-              checked={this.state.isGoing}
+              name='start'
+              type='number'
+              value={start}
               placeholder={'Chiffres entre 1 et 20'}
               onChange={this.handleOriginChange} />
           </label>
@@ -60,23 +117,23 @@ class InputCase extends Component {
           <label>
             Destination : 
             <input
-              name="end"
-              type="number"
-              value={this.state.numberOfGuests}
+              name='end'
+              type='number'
+              value={end}
               placeholder={'Chiffres entre 1 et 20'}
               onChange={this.handleDestinatioonChange} />
           </label>
           <br />
           <label>
             Type de trajet : 
-            <select value={this.state.value} onChange={this.handleJourneyTypeChange}>
-              <option value="BrutPower">Tous chemins</option>
-              <option value="Dijkstra">Le plus rapide</option>
-              <option value="FordFerkuson">Le plus court</option>
+            <select value={methode} onChange={this.handleJourneyTypeChange}>
+              <option value='BrutPower'>Tous chemins</option>
+              <option value='Dijkstra'>Le plus rapide</option>
+              <option value='FordFerkuson'>Le plus court</option>
             </select>
           </label>
           <br />
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit" onSubmit={this.handleSubmit}/>
         </form>
       );
     }
